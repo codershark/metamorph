2e7422df4d2a3826c563a947f06348f11c70f3be
codershark <61663100+codershark@users.noreply.github.com>
2020-03-22 10:56:56 -0700

    Update topological_sort.cpp

--- topics/graph/topological_sort.cpp
+++ topics/graph/topological_sort.cpp
@@ -1,58 +1,51 @@
-// Source : https://oj.leetcode.com/problems/course-schedule/
+// Source : https://oj.leetcode.com/problems/course-schedule-ii/
 // Author : Sukriti
 // Date   : 2020-03-01
 
 
 /********************************************************************************** 
 * 
-* There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
-* 
+* There are a total of n courses you have to take, labeled from 0 to n-1.
 * Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+* Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
 * 
-* Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
-* 
-*  
+* There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
 * 
 * Example 1:
 * 
-* Input: numCourses = 2, prerequisites = [[1,0]]
-* Output: true
-* Explanation: There are a total of 2 courses to take. 
-*              To take course 1 you should have finished course 0. So it is possible.
+* Input: 2, [[1,0]] 
+* Output: [0,1]
+* Explanation: There are a total of 2 courses to take. To take course 1 you should have finished   
+*              course 0. So the correct course order is [0,1] .
 *               
 **********************************************************************************/
 
 class Solution {
 public:
-    class GNode {
-    public:
+    struct GNode {
         vector<int> neighbors;
         int in_degree = 0;
     };
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        if (prerequisites.size() == 0) return true;
-        unordered_map<int, GNode> graph;
-        for (auto entry : prerequisites) {
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<GNode> graph(numCourses);
+        for (const auto& entry : prerequisites) {
             graph[entry[1]].neighbors.push_back(entry[0]);
             graph[entry[0]].in_degree++;
         }
-        queue<int> zero_dependency;
-        for (auto entry : graph) {
-            if (entry.second.in_degree > 0) continue;
-            zero_dependency.push(entry.first);
+        queue<int> q;
+        for (int i = 0; i < graph.size(); ++i) {
+            if (graph[i].in_degree == 0) q.push(i);
         }
-        int removedEdges = 0;
-        while (!zero_dependency.empty()) {
-            auto temp = zero_dependency.front();
-            zero_dependency.pop();
+        vector<int> result;
+        while (!q.empty()) {
+            auto temp = q.front();
+            q.pop();
+            result.push_back(temp);
             for (auto n : graph[temp].neighbors) {
                 graph[n].in_degree--;
-                removedEdges++;
-                if (graph[n].in_degree == 0) {
-                    zero_dependency.push(n);
-                }
+                if (graph[n].in_degree == 0) q.push(n);
             }
         }
-        return removedEdges == prerequisites.size();
+        return result.size() == numCourses ? result : vector<int>(0);
     }
 };
